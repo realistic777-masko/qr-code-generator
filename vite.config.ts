fc3a5ada088c99b6506d7dceb55bee80bf586443
realistic777-masko <manassehkono2580@gmail.com>
2026-09-16 00:52:19 +0000

    Set base path for QR code generator

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: `/qr-code-generator/`
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
