51556e7fb6d82e5cc5c14f0dfe788bac99625b1a
realistic777-masko <manassehkono2580@gmail.com>
2026-09-17 01:08:06 +0000

    Add HelpSection component to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { AboutSection } from './components/AboutSection';
 import { Footer } from './components/Footer';
 import { QRConfig, QRState, QRType } from './types';
 import { getQRPayload } from './utils/qrHelper';
+import {HelpSection } from ' ./components/HelpSection';
 
 const INITIAL_STATE: QRState = {
   type: 'url',
@@ -124,7 +125,7 @@ export default function App() {
           </div>
         </div>
         {/* Help & Guides */}
-        {/* HelpSection /> */}
+        <HelpSection /> 
 
         {/* SEO & Educational About Section */}
         <button 
